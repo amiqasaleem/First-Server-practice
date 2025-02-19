285df63881ad0b55e1dffe7713b1a28d8784847f
amiqasaleem <amiqasaleem@gmail.com>
2025-02-19 10:35:19 +0500

    changes

--- app.mjs
+++ app.mjs
@@ -1,10 +1,26 @@
-import express from 'express'
+import express from "express"
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT ||  3000
+import path from 'path'
+const __dirname = path.resolve()
+
+
+app.use('/', express.static(path.join(__dirname, './website/dist')))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("get  chlrha ha")
 })
+
+app.get('/weather', (req, res) => {
+  res.json({ user: 'tobi' })
+})
+app.get('/weather/:cityName', (req, res) => {
+  res.send(req.params)
+})
+app.get('/weather/:cityName/:side', (req, res) => {
+  res.send(req.params)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
